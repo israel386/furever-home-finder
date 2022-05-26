b6c5e5f862bdc5197590a0488842d931e5aa4d26
israel386 <israel_delarosa@knights.ucf.edu>
2022-05-26 18:12:29 -0400

    Updated dog-fact.js

---
+++ assets/js/dog-facts.js
@@ -0,0 +1,22 @@
+fetch(
+    // TODO: Add query parameters to the URL such that the number of issues returned is limited to 10.
+    // TODO: Add a `sort` parameter to sort the issues by `createdAt` in descending order.
+    // Hint: use & to join multiple query parameters. Use `=` to join key and value.
+    'https://cat-fact.herokuapp.com/facts'
+)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+        console.log(data);
+        for (var i = 0; i < data.length; i++) {
+            var catFact = data[i].text
+
+            var ex = document.querySelector("#available-dogs")
+            var sdf = document.createElement("div")
+
+            sdf.innerText = catFact;
+
+
+        }
+    });
