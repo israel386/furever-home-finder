9e7636b851603207f91cb991c3243100eda8fba8
israel386 <israel_delarosa@knights.ucf.edu>
2022-05-30 15:01:17 -0400

    Updated modal

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,102 +1,130 @@
-var searchButton=document.getElementById("searchButton")
+var searchButton = document.getElementById("searchButton")
 var key = 'dKli27YVN1Xw6LHvN9NAyhYR5dEydVxXXojInFquj2ZIyetfC6'
 var secret = 'x2rtjtX1uuWWLBsSomBt1QHVNtXtjTBnL5EoGWLr'
 var token, tokenType;
+var tag = document.createElement("p")
+var element = document.getElementById("cat-fact");
+element.appendChild(tag)
 
-document.addEventListener('DOMContentLoaded', function() {
-  var elems = document.querySelectorAll('.modal');
-  var instances = M.Modal.init(elems);
+document.addEventListener('DOMContentLoaded', function () {
+	var elems = document.querySelectorAll('.modal');
+	var instances = M.Modal.init(elems);
+
 });
 
-function getAccessToken () {
-    fetch('https://api.petfinder.com/v2/oauth2/token', {
-        method: 'POST',
-        body: 'grant_type=client_credentials&client_id=' + key + '&client_secret=' + secret,
-        headers: {
-            'Content-Type': 'application/x-www-form-urlencoded'
-        }
-    }).then(function (response) {
-        // Return the response as JSON
-        return response.json();
-    }).then(function (data) {
-        // Log the API data
-        console.log('token', data);
-        // Store token data
-        token = data.access_token;
-        tokenType = data.token_type;
-    }).catch(function(err) {
-        console.log(err);
-    });
+function getCatFact() {
+	fetch(
+		'https://cat-fact.herokuapp.com/facts'
+	)
+		.then(function (response) {
+			return response.json();
+		})
+		.then(function (data) {
+			console.log(data);
+			for (var i = 0; i < data.length; i++) {
+
+				tag.textContent = data[Math.floor(Math.random() * data.length)].text
+
+				// var text = document.createTextNode(data[Math.floor(Math.random() * data.length)].text);
+				// tag.appendChild(text);
+				var removeFact = document.getElementById("#cat-fact")
+				removeFact.parentElement.removeChild(tag)
+
+				return ++i
+			}
+		});
+}
+getCatFact()
+
+function getAccessToken() {
+	fetch('https://api.petfinder.com/v2/oauth2/token', {
+		method: 'POST',
+		body: 'grant_type=client_credentials&client_id=' + key + '&client_secret=' + secret,
+		headers: {
+			'Content-Type': 'application/x-www-form-urlencoded'
+		}
+	}).then(function (response) {
+		// Return the response as JSON
+		return response.json();
+	}).then(function (data) {
+		// Log the API data
+		console.log('token', data);
+		// Store token data
+		token = data.access_token;
+		tokenType = data.token_type;
+	}).catch(function (err) {
+		console.log(err);
+	});
 };
 
-setInterval(function(){refreshAccessToken(); }, 1800000);
+setInterval(function () { refreshAccessToken(); }, 1800000);
 
 function refreshAccessToken() {
-    fetch('https://api.petfinder.com/v2/oauth2/token', {
-        method: 'POST',
-        body: 'grant_type=client_credentials&client_id=' + key + '&client_secret=' + secret,
-        headers: {
-            'Content-Type': 'application/x-www-form-urlencoded'
-        }
-    }).then(function (response) {
-        // Return the response as JSON
-        return response.json();
-    }).then(function (data) {
-        // Log the API data
-        console.log('token', data);
-        // Store token data
-        token = data.access_token;
-        tokenType = data.token_type;
-    }).catch(function(err) {
-        console.log(err);
-    });
-  }
+	fetch('https://api.petfinder.com/v2/oauth2/token', {
+		method: 'POST',
+		body: 'grant_type=client_credentials&client_id=' + key + '&client_secret=' + secret,
+		headers: {
+			'Content-Type': 'application/x-www-form-urlencoded'
+		}
+	}).then(function (response) {
+		// Return the response as JSON
+		return response.json();
+	}).then(function (data) {
+		// Log the API data
+		console.log('token', data);
+		// Store token data
+		token = data.access_token;
+		tokenType = data.token_type;
+	}).catch(function (err) {
+		console.log(err);
+	});
+}
 // the next line and function set up the button in our html to be clickable and reactive 
 document.addEventListener('DOMContentLoaded', bindButtons);
 
-function bindButtons(){
-    document.getElementById('searchButton').addEventListener('click', function(event){
-      event.preventDefault();
-      var zip = document.getElementById('zip-code').value; // this line gets the zip code from the form entry
-      var apiurl = 'https://api.petfinder.com/v2/animals';
-      
-      fetch(apiurl, {
-        method: "GET", 
-        headers: {
-          'Content-Type': 'application/json',
-          Authorization: 'Bearer ' + token
-        },
-        
-      }).then(function(response) {
-        return response.json();
-      }).then(function(data) {
-        console.log(data);
-        for (var i = 0; i <data.length; i++) {
-          var name = data[i].petfinder.pet.name.$t;
-          var img = data[i].petfinder.pet.media.photos.photo[0].$t;
-          var id = data[i].petfinder.pet.id.$t;
-    
-          var newName = document.createElement('a');
-          var animalsList = document.getElementById('animals-list');
-          newName.textContent = name;
-          newName.href = 'https://www.petfinder.com/v2/animals' + id;
-    
-          var newImg = document.createElement('img');
-          newImg.src = img;
-          newImg.setAttribute('style', 'max-width: 200px')
-          var animal = document.createElement("li");
-          // list.setAttribute("id", "List");
-          // document.body.appendChild(list);
-          animalsList.appendChild(animal)
-          animal.appendChild(newName)
-          animal.appendChild(newImg);
-        
-      }
-    })
-  
-    
-});
-  }
+function bindButtons() {
+	document.getElementById('searchButton').addEventListener('click', function (event) {
+		event.preventDefault();
+		var zip = document.getElementById('zip-code').value; // this line gets the zip code from the form entry
+		var apiurl = 'https://api.petfinder.com/v2/animals';
+
+		fetch(apiurl, {
+			method: "GET",
+			headers: {
+				'Content-Type': 'application/json',
+				Authorization: 'Bearer ' + token
+			},
+
+		}).then(function (response) {
+			return response.json();
+		}).then(function (data) {
+			console.log(data);
+			for (var i = 0; i < data.length; i++) {
+				var name = data[i].petfinder.pet.name.$t;
+				var img = data[i].petfinder.pet.media.photos.photo[0].$t;
+				var id = data[i].petfinder.pet.id.$t;
+
+				var newName = document.createElement('a');
+				var animalsList = document.getElementById('animals-list');
+				newName.textContent = name;
+				newName.href = 'https://www.petfinder.com/v2/animals' + id;
+
+				var newImg = document.createElement('img');
+				newImg.src = img;
+				newImg.setAttribute('style', 'max-width: 200px')
+				var animal = document.createElement("li");
+				// list.setAttribute("id", "List");
+				// document.body.appendChild(list);
+				animalsList.appendChild(animal)
+				animal.appendChild(newName)
+				animal.appendChild(newImg);
+
+			}
+		})
+
+
+	});
+}
 
 getAccessToken();
 // function displayResults (){
